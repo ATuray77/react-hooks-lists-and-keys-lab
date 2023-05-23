0e1f37e8914bf13643cfe4012c5ad6898f68367b
Aruna Turay <tura_arun@yahoo.com>
2023-05-23 13:56:52 -0400

    fixed navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,22 +3,23 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  // const linksItems = links.map((linkObj) => {
-  //   return <a key = {linkObj} {`#${linkObj}`}></a>
-  // })
+  const mylinks = links.map((eachLink) => {
+    return <a key={eachLink} href={"#" + eachLink}>{eachLink}</a>
+  })
+  return (
+    <nav>
+      {mylinks}
+    </nav>
+  )
+  
   
   // return (
-    
-  //   return <nam>{linksItems}</nam>
- 
-  // )
-  return (
-   <nav>
-     <a href="#home">home</a>
-     <a href="#about">about</a>
-     <a href="#projects">projects</a>
-   </nav>
-  ) 
+  //  <nav>
+  //    <a href="#home">home</a>
+  //    <a href="#about">about</a>
+  //    <a href="#projects">projects</a>
+  //  </nav>
+  // ) 
 }
 
 
